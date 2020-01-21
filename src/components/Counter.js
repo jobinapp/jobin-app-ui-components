f1261b4d6db1f4a32bbc43302671088d1beed7bd
Guille <g.angeles@jobin.es>
2020-01-21 13:38:10 +0100

    Counter UI update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import CustomText from "./CustomText";
+import { lineColor } from "jobin-app-ui-components/src/constants/color";
 
 const View = styled.View`
     display: flex;
-    flex-direction: row;
     background-color: #fff;
     height: 48px;
     justify-content: center;
@@ -16,18 +16,31 @@ const ButtonRight = styled.TouchableOpacity`
     border: 0px;
     opacity: ${props => (props.disabled ? 0.5 : 1.0)};
     background-color: transparent;
-    margin-left: 16px;
+    margin-left: 4px;
 `;
 const ButtonLeft = styled.TouchableOpacity`
     border: 0px;
     opacity: ${props => (props.disabled ? 0.5 : 1.0)};
     background-color: transparent;
-    margin-right: 16px;
+    margin-right: 4px;
 `;
 const ButtonImage = styled.Image`
     height: 36px;
     width: 36px;
 `;
+const AmountView = styled.View`
+    justify-content: center;
+    align-items: center;
+    height: 36px;
+    width: 80px;
+    border-color: ${lineColor};
+    border-width: 1px;
+    border-radius: 16px;
+    margin-bottom: 8px;
+`
+const ButtonsView = styled.View`
+    flex-direction: row;
+`
 
 const Counter = props => {
     const [counter, setCounter] = useState(props.min || 0);
@@ -59,27 +72,31 @@ const Counter = props => {
 
     return (
         <View style={props.style}>
-            <ButtonLeft
-                key={"button-left"}
-                onPress={disabledLeft ? null : substractToCounter}
-                disabled={disabledLeft}
-            >
-                <ButtonImage
-                    source={require("../assets/images/common/substractButton.png")}
-                />
-            </ButtonLeft>
-            <CustomText key={"input"} type="semibold">
-                {counter}
-            </CustomText>
-            <ButtonRight
-                key={"button-right"}
-                onPress={disabledRight ? null : addToCounter}
-                disabled={disabledRight}
-            >
-                <ButtonImage
-                    source={require("../assets/images/common/addButton.png")}
-                />
-            </ButtonRight>
+            <AmountView>
+                <CustomText key={"input"} type="semibold">
+                    {counter}
+                </CustomText>
+            </AmountView>
+            <ButtonsView>
+                <ButtonLeft
+                    key={"button-left"}
+                    onPress={disabledLeft ? null : substractToCounter}
+                    disabled={disabledLeft}
+                >
+                    <ButtonImage
+                        source={require("../assets/images/common/substractButton.png")}
+                    />
+                </ButtonLeft>
+                <ButtonRight
+                    key={"button-right"}
+                    onPress={disabledRight ? null : addToCounter}
+                    disabled={disabledRight}
+                >
+                    <ButtonImage
+                        source={require("../assets/images/common/addButton.png")}
+                    />
+                </ButtonRight>
+            </ButtonsView>
         </View>
     );
 };
