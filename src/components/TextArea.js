58ce37156c41c4d9a326dc4da3d0f00d109e0404
Ricardo Veronese Ricci <r.veronese@jobin.es>
2020-01-22 17:42:11 +0100

    TextArea with tooltips is done

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -8,46 +8,62 @@ import {
     redJobinColor
 } from "../constants/color";
 import { regular } from "../constants/fonts";
+import { marginLeft, marginRight } from "../constants/mainConstants";
 
 const View = styled.View`
-    height: 58px;
-    border-bottom-width: 1px;
-    border-bottom-color: ${props =>
-        props.badInput ? redJobinColor : lineColor};
+    margin-left: ${marginLeft};
+    margin-right: ${marginRight};
+    margin-top: 16px;
+    margin-bottom: 16px;
+    height: 56px;
+    border-width: 1px;
+    border-color: ${props => (props.badInput ? redJobinColor : lineColor)};
     ${props => props.style};
 `;
 const TextInput = styled.TextInput`
-    height: 22px;
     font-size: 16px;
+    padding: 8px 8px;
     font-family: ${regular};
 `;
 
 const TextArea = props => {
     const { style, badInput, onChangeText, ...other } = props;
+    const tooltip = props.tooltip || 1000;
     const [value, setValue] = useState(null);
+    const [chars, setChars] = useState(tooltip);
 
     const onChange = text => {
         if (text.length > 0) setValue(text);
         else setValue(null);
         props.onChangeText ? props.onChangeText(text) : null;
+        setChars(tooltip - text.length);
     };
 
     return (
-        <View style={style} badInput={badInput}>
+        <>
+            <View style={style} badInput={badInput}>
+                <CustomText
+                    style={{ marginTop: 13 }}
+                    fontSize={12}
+                    textColor={deactivatedGrayColor}
+                >
+                    {props.placeholder && value ? props.placeholder : ""}
+                </CustomText>
+                <TextInput
+                    onChangeText={onChange}
+                    {...other}
+                    multiline={true}
+                    numberOfLines={props.numberOfLines || 4}
+                />
+            </View>
             <CustomText
-                style={{ marginTop: 13 }}
-                fontSize={12}
+                style={{ marginLeft: marginLeft }}
+                fontSize={14}
                 textColor={deactivatedGrayColor}
             >
-                {props.placeholder && value ? props.placeholder : ""}
+                {`${chars} caracter${chars >= 2 ? "es" : ""}`}
             </CustomText>
-            <TextInput
-                onChangeText={onChange}
-                {...other}
-                multiline={true}
-                numberOfLines={props.numberOfLines || 4}
-            />
-        </View>
+        </>
     );
 };
 
