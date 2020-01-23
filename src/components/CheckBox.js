ad052e1d10d541f3552e870b78bc88a62ad4dbf5
Guille <g.angeles@jobin.es>
2020-01-23 12:22:00 +0100

    Checkbox login and UI

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,29 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 import CustomText from "./CustomText";
 
-import { lineColor, backgroundGrayColor, whiteColor } from "../constants/color";
+import { whiteColor } from "../constants/color";
 
 const Container = styled.TouchableOpacity`
-    display: flex;
-    flex: 1;
     flex-direction: row;
-    height: 56px;
-    align-items: center;
-    background-color: ${props =>
-        props.selected ? backgroundGrayColor : whiteColor};
+    background-color: ${whiteColor};
     ${props => props.style};
 `;
 const TextView = styled.View`
-    display: flex;
     flex: 1;
-    height: 100%;
     margin-right: 24px;
     margin-left: 12px;
-    justify-content: center;
-    border-bottom-color: ${lineColor};
-    border-bottom-width: ${props => (props.selected ? 0 : 1)};
 `;
 
 const CheckImage = styled.Image`
@@ -32,16 +22,24 @@ const CheckImage = styled.Image`
 `;
 
 const CheckBox = props => {
+
+    const [selected, setSelected] = useState(props.selected ? props.selected : false);
+
+    const onChange = () =>{
+        setSelected(!selected);
+        props.onChange ? props.onChange(!selected) : null;
+    }
+
     return (
-        <Container style={props.style} {...props}>
+        <Container style={props.style} onPress={onChange}>
             <CheckImage
                 source={
-                    props.selected
+                    selected
                         ? require("../assets/images/common/checkboxOn.png")
                         : require("../assets/images/common/checkboxOff.png")
                 }
             />
-            <TextView selected={props.selected}>
+            <TextView selected={selected}>
                 <CustomText>{props.text}</CustomText>
             </TextView>
         </Container>
